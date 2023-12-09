07b269152d7fdac409e88e85789183d1bedda4d2
jjberk1234 <jjberkel.8@gmail.com>
2023-12-08 17:48:20 -0800

    Fixed run_CNN

--- run_CNN.py
+++ run_CNN.py
@@ -1,16 +1,15 @@
 import datasplit
-from test import test
-from train_ import train
+from train_CNN import test
 import torch
 from sklearn.metrics import precision_score,confusion_matrix,accuracy_score
 
 # Load data
-dataset_path = "path to dataset"
+dataset_path = "C:\\Users\\blues\\OneDrive\\Desktop\\UCI\\Fall '23\\CS 184A\\all_train_data"
 data = datasplit.process_data(dataset_path)
-data_loader = torch.utils.data.ConcatDataset(data)
+data_loader =  torch.utils.data.DataLoader(dataset=data, shuffle=True)
 
 # Load model
-model = torch.load("path to CNN model")
+model = torch.load("C:\\Users\\blues\\OneDrive\\Desktop\\UCI\\Fall '23\\CS 184A\\Models\\CNNmodel_full_2.pt")
 
 # Print metrics
 accuracy, precision, confusion = test(model, data_loader)
